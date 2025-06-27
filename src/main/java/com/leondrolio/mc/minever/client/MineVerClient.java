1e3a1a559ba3d55ffe79480020724e56d635d63c
FallingUser <1265336243@qq.com>
2025-06-27 18:47:30 +0800

    Move src/main/resources/java/.../MineVerClient.java directory to src/main/java/.../MineVerClient.java

---
+++ src/main/java/com/leondrolio/mc/minever/client/MineVerClient.java
@@ -0,0 +1,11 @@
+package com.leondrolio.mc.minever.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class MineVerClient implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+    System.out.println("Client initialised!");
+  }
+}
