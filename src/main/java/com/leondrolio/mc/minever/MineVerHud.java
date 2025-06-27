74b5fdcbb2112af19c536fc5110b7419834b9ddb
FallingUser <1265336243@qq.com>
2025-06-27 18:48:39 +0800

    Add files via upload

---
+++ src/main/java/com/leondrolio/mc/minever/MineVerHud.java
@@ -0,0 +1,25 @@
+package com.leondrolio.mc.minever.mixin;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.text.Text;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class MineVerHud {
+  @Inject(method = "render", at = @At("RETURN"))
+  public void onRender(DrawContext context, RenderTickCounter tickDelta, CallbackInfo info) {
+    MinecraftClient client = MinecraftClient.getInstance();
+    if (!client.getDebugHud().shouldShowDebugHud()) {
+      String version = SharedConstants.getGameVersion().getName();
+      Text text = Text.literal(String.format("Minecraft %s", version));
+      context.drawTextWithShadow(client.textRenderer, text, 2, 2, 0xFFFFFF);
+    }
+  }
+}
