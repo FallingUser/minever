82f1e44a6c6dcb274b0c9ed29fdecb31ccb61815
FallingUser <1265336243@qq.com>
2025-07-01 10:48:02 +0800

    Update for Minecraft Java Edition 1.21.6

--- src/main/java/com/leondrolio/mc/minever/client/MineVerClient.java
+++ src/main/java/com/leondrolio/mc/minever/client/MineVerClient.java
@@ -1,11 +1,22 @@
 package com.leondrolio.mc.minever.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.minecraft.SharedConstants;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
 
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class MineVerClient implements ClientModInitializer {
   @Override
   public void onInitializeClient() {
-    System.out.println("Client initialised!");
+    System.out.println("Installed MineVer Mod Successfully!");
+    HudElementRegistry.addLast(Identifier.of("minever", "version"), (context, tickCounter) -> {
+      String version = SharedConstants.getGameVersion().id();
+      Text text = Text.literal(String.format("Minecraft %s", version));
+      context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, text, 2, 2, -1);
+    });
   }
 }
